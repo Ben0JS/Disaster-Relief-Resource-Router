677999af12af7b6b020a42c09c6a03102208db88
BEN <vencherbeno1229@gmail.com>
2025-11-12 00:47:14 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://disaster-relief-resource-router.onrender.com/",
 });
 
 // attach token to every request
